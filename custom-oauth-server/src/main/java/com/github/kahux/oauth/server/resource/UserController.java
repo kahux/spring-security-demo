6ad745cf3a1245b1e4ab3765e0821db4d5ee5489
Miue Hu <yiliuxim@163.com>
2017-09-19 15:39:20 +0800

    add resources for resources server

---
+++ custom-oauth-server/src/main/java/com/github/kahux/oauth/server/resource/UserController.java
@@ -0,0 +1,18 @@
+package com.github.kahux.oauth.server.resource;
+
+import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+  @RequestMapping({ "/user", "/me" })
+  public Map<String, String> user(Principal principal) {
+    Map<String, String> map = new LinkedHashMap<>();
+    map.put("name", principal.getName());
+    return map;
+  }
+}
