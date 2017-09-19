a18f1efcd4cddeea14f5055a282076f49587b9d4
Miue Hu <yiliuxim@163.com>
2017-09-19 14:55:11 +0800

    implement simple oauth server

---
+++ custom-oauth-server/src/main/java/com/github/kahux/oauth/server/CustomOauthServerApplication.java
@@ -0,0 +1,19 @@
+package com.github.kahux.oauth.server;
+
+import org.springframework.boot.Banner.Mode;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@EnableAuthorizationServer
+@SpringBootApplication
+public class CustomOauthServerApplication {
+
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(CustomOauthServerApplication.class)
+        .bannerMode(Mode.OFF)
+        .web(true)
+        .run(args);
+  }
+
+}
