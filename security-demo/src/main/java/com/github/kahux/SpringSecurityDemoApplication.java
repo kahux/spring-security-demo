7abaab36f9ffabd61d0ba72ee4fc063456e7686b
Miue Hu <yiliuxim@163.com>
2017-09-19 13:48:08 +0800

    create oauth server module

---
+++ security-demo/src/main/java/com/github/kahux/SpringSecurityDemoApplication.java
@@ -0,0 +1,21 @@
+package com.github.kahux;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SpringSecurityDemoApplication {
+
+  /**
+   * Application entry.
+   *
+   * @param args  command line parameters
+   */
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(SpringSecurityDemoApplication.class)
+        .bannerMode(Banner.Mode.OFF)
+        .web(true)
+        .run(args);
+  }
+}
